732b1e05fe5ca210018b8a8eff84b32c2e1c71ab
Joy Lin <vvlnote@hotmail.com>
2019-10-01 21:33:42 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,14 +2,14 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'pry'
 class InteractiveRecord
-  
+
   def self.table_name
     self.to_s.downcase.pluralize
   end
-  
+
   def self.column_names
     DB[:conn].results_as_hash = true
-    
+
     sql = "PRAGMA table_info('#{table_name}')"
     table_info = DB[:conn].execute(sql)
     column_names = []
@@ -18,48 +18,53 @@ class InteractiveRecord
     end
     column_names.compact
   end
-  
+
   def initialize(options={})
     options.each do |property, value|
       self.send("#{property}=", value)
     end
   end
-  
+
   def save
     #DB[:conn].results_as_hash = true
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
-  
+
   def table_name_for_insert
     self.class.table_name
   end
-  
+
   def col_names_for_insert
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
-  
+
   def values_for_insert
     values = []
     self.class.column_names.each do |col_name|
       values << "'#{send(col_name)}'" unless send(col_name).nil?
     end
-    
+
     values.join(", ")
   end
-  
+
   def self.find_by_name(name)
-    DB[:conn].results_as_hash = true
-    sql = "SELECT * FROM students WHERE name = '#{name}'"
-    
+
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+
     DB[:conn].execute(sql)
   end
-  
-  def self.find_by(options={})
-    sql = "SELECT * FROM students WHERE #{options.keys[0]} = #{options.values[0]}"
-    
-    DB[:conn].execute(sql)
-      
+
+  def self.find_by(attribute)
+    column_name = attribute.keys[0].to_s
+    value_name = attribute.values[0]
+
+    sql = <<-SQL
+      SELECT * FROM #{table_name} WHERE #{column_name} = ?
+      SQL
+
+    DB[:conn].execute(sql,value_name)
+
   end
 end
